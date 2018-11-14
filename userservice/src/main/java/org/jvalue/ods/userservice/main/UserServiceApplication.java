b0ac9022396869e992910a4427001104c7d1dd50
Georg Schwarz <georg.schwarz@fau.de>
2018-11-14 09:51:36 +0100

    setup of new gradle project for user service

---
+++ userservice/src/main/java/org/jvalue/ods/userservice/main/UserServiceApplication.java
@@ -0,0 +1,81 @@
+package org.jvalue.ods.userservice.main;
+
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import io.dropwizard.Application;
+import io.dropwizard.jersey.DropwizardResourceConfig;
+import io.dropwizard.jersey.setup.JerseyContainerHolder;
+import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.hibernate.validator.HibernateValidator;
+import org.hibernate.validator.HibernateValidatorConfiguration;
+import org.hibernate.validator.cfg.ConstraintMapping;
+import org.jvalue.ods.userservice.utils.GuiceConstraintValidatorFactory;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.validation.Validation;
+import javax.ws.rs.core.Context;
+import java.util.EnumSet;
+
+public final class UserServiceApplication extends Application<UserServiceConfig> {
+
+
+	public static void main(String[] args) throws Exception {
+		new UserServiceApplication().run(args);
+	}
+
+
+	@Override
+	public String getName() {
+		return "ODS UserService";
+	}
+
+
+	@Override
+	@Context
+	public void run(UserServiceConfig configuration, Environment environment) {
+
+		Injector injector = Guice.createInjector(
+				//TODO
+			);
+
+		// start data grabbing
+		// TODO
+
+		// setup users
+		// TODO
+
+		// setup health checks
+		// TODO
+
+		// setup Cross-Origin Resource Sharing (CORS)
+		final FilterRegistration.Dynamic corsFilter =
+			environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+		corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM,
+			"X-Requested-With,Content-Type,Accept,Origin,Authorization");
+		corsFilter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+		corsFilter.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+		corsFilter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+		// configure administration
+		DropwizardResourceConfig jerseyConfig = new DropwizardResourceConfig(environment.metrics());
+		JerseyContainerHolder jerseyContainerHolder = new JerseyContainerHolder(new ServletContainer(jerseyConfig));
+		//TODO
+
+		// setup validation for external classes
+		HibernateValidatorConfiguration validatorContext = Validation.byProvider(HibernateValidator.class).configure();
+		ConstraintMapping mapping = validatorContext.createConstraintMapping();
+		//TODO
+
+		// setup Guice DI for hibernate validator
+		environment.setValidator(validatorContext.addMapping(mapping)
+				.constraintValidatorFactory(new GuiceConstraintValidatorFactory(injector))
+				.buildValidatorFactory()
+				.getValidator());
+	}
+}
---
+++ userservice/src/main/java/org/jvalue/ods/userservice/main/UserServiceConfig.java
@@ -0,0 +1,9 @@
+package org.jvalue.ods.userservice.main;
+
+import io.dropwizard.Configuration;
+
+
+public final class UserServiceConfig extends Configuration {
+
+
+}

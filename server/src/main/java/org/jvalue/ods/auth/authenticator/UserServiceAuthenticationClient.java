6d843368ab0201bdba67713d9c535f91862173a1
Georg Schwarz <georg.schwarz@fau.de>
2018-11-26 14:17:37 +0100

    retry authentication request to userservice if it failes due to connection

--- server/src/main/java/org/jvalue/ods/auth/authenticator/UserServiceAuthenticationClient.java
+++ server/src/main/java/org/jvalue/ods/auth/authenticator/UserServiceAuthenticationClient.java
@@ -1,35 +1,46 @@
 package org.jvalue.ods.auth.authenticator;
 
 import com.google.common.base.Optional;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.RetryPolicy;
 import org.jvalue.ods.auth.User;
 import org.jvalue.ods.auth.config.AuthConfig;
-import org.jvalue.ods.communication.messaging.UserEvent;
-import org.jvalue.ods.communication.messaging.UserEventConsumer;
-import org.jvalue.ods.communication.messaging.UserEventHandler;
 
 import javax.inject.Inject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import java.net.ConnectException;
+import java.net.SocketException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class UserServiceAuthenticationClient implements RemoteAuthenticationClient {
 
 	public final String userServicePath;
+
+	private RetryPolicy retryPolicy;
 
 	@Inject
 	public UserServiceAuthenticationClient(
 		AuthConfig authConfig
 	) {
 		userServicePath = authConfig.getUserServiceUrl();
+
+		retryPolicy = new RetryPolicy()
+			.retryOn(List.of(ConnectException.class, SocketException.class))
+			.withDelay(1, TimeUnit.SECONDS)
+			.withMaxRetries(3);
 	}
 
 	@Override
 	public Optional<User> authenticate(String authHeader) {
+		return Failsafe.with(retryPolicy).get(() -> requestUser(authHeader));
+	}
 
+	private Optional<User> requestUser(String authHeader) {
 		Client client = ClientBuilder.newClient();
-		// TODO: make retries if no connection
-
 		return Optional.of(
 			client
 				.target(userServicePath + "/users/me")
